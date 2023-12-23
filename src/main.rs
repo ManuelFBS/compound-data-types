d98803e3e300ede536000b57850475fc3358c1e8
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-23 05:13:25 -0400

    Creando main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(PartialEq, Debug)]
+// Se declara una struct Car que describe un vehículo con 4 campos...
+struct Car {
+    color: String,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32)
 }
+
+#[derive(PartialEq, Debug)]
+// Se declara un enum para el tipo de transmisión del Car...
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic
+}
+
+#[derive(PartialEq, Debug)]
+// Se declara un enum Age para Car age...
+enum Age {
+    New,
+    Used
+}
+
